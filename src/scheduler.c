78be23f3556fcfd4d3ad0dd50d00d81a185f49b6
nihiL7331 <pujanekp6@gmail.com>
2026-05-09 00:08:47 +0200

    refactor(scheduler) thread -> thrd rename

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,3 +1,4 @@
+#include "scheduler.h"
 #include <thrd_ndl/thrd_ndl.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -7,7 +8,7 @@
 
 extern void thrd_ndl_switch(tcb_t* old_tcb, tcb_t* new_tcb);
 
-static tcb_t* curr_thread = NULL;
+static tcb_t* curr_thrd = NULL;
 static tcb_t* rdy_queue_hd = NULL;
 static tcb_t* rdy_queue_tl = NULL;
 
@@ -15,19 +16,19 @@ static tcb_t* sleep_queue_hd = NULL;
 
 void thrd_yield(void) {
   // instantly update the state if the thrd was running
-  if (curr_thread->state ==  THRD_RUNNING) {
-    curr_thread->state = THRD_READY;
-    thrd_enqueue(curr_thread, &rdy_queue_hd, &rdy_queue_tl);
+  if (curr_thrd->state ==  THRD_RUNNING) {
+    curr_thrd->state = THRD_READY;
+    thrd_enqueue(curr_thrd, &rdy_queue_hd, &rdy_queue_tl);
   }
 
   // if the thread that has the closest 'wakeup_time'
   // is waking up, then pop it off the sleep queue
   uint64_t curr_time_ms = get_os_time();
   while (sleep_queue_hd != NULL && curr_time_ms >= sleep_queue_hd->wakeup_time) {
-    tcb_t* awake_thread = sleep_queue_hd;
+    tcb_t* awake_thrd = sleep_queue_hd;
     sleep_queue_hd = sleep_queue_hd->next;
-    awake_thread->state = THRD_READY;
-    thrd_enqueue(awake_thread, &rdy_queue_hd, &rdy_queue_tl);
+    awake_thrd->state = THRD_READY;
+    thrd_enqueue(awake_thrd, &rdy_queue_hd, &rdy_queue_tl);
   }
 
   // there's no one else waiting,
@@ -39,90 +40,90 @@ void thrd_yield(void) {
       // then go back to the top of the function
       thrd_yield();
       return;
-    } else if (curr_thread->state == THRD_DEAD) // all threads are dead, close the program
+    } else if (curr_thrd->state == THRD_DEAD) // all threads are dead, close the program
       exit(0);
     else // all threads are sleeping / UB
       exit(1);
   }
 
   // pop the head
-  tcb_t* next_thread = thrd_dequeue(&rdy_queue_hd, &rdy_queue_tl);
+  tcb_t* next_thrd = thrd_dequeue(&rdy_queue_hd, &rdy_queue_tl);
 
-  // set 'next_thread' as 'curr_thread'
-  tcb_t* old_thread = curr_thread;
-  curr_thread = next_thread;
-  curr_thread->state = THRD_RUNNING;
+  // set 'next_thrd' as 'curr_thrd'
+  tcb_t* old_thrd = curr_thrd;
+  curr_thrd = next_thrd;
+  curr_thrd->state = THRD_RUNNING;
 
   // call the asm context switch procedure
-  thrd_ndl_switch(old_thread, curr_thread);
+  thrd_ndl_switch(old_thrd, curr_thrd);
 }
 
 void thrd_init(void) {
   // if the thread is already initialized, just return
-  if (curr_thread != NULL)
+  if (curr_thrd != NULL)
     return;
 
   // make a dummy thread
-  tcb_t* init_thread = (tcb_t*)malloc(sizeof(tcb_t));
-  init_thread->bsp = NULL;
-  init_thread->rsp = NULL;
-  init_thread->next = NULL;
-  init_thread->state = THRD_RUNNING;
+  tcb_t* init_thrd = (tcb_t*)malloc(sizeof(tcb_t));
+  init_thrd->bsp = NULL;
+  init_thrd->rsp = NULL;
+  init_thrd->next = NULL;
+  init_thrd->state = THRD_RUNNING;
 
-  curr_thread = init_thread;
+  curr_thrd = init_thrd;
 }
 
 int thrd_create(thrd_t* out_thread, void (*func)(void)) {
-  tcb_t* new_thread = tcb_init(func);
-  if (new_thread == NULL)
+  tcb_t* new_thrd = tcb_init(func);
+  if (new_thrd == NULL)
     return THRD_OOM;
 
   // pass the address to the pointer given by the user
   if (out_thread != NULL)
-    *out_thread = (thrd_t)new_thread;
+    *out_thread = (thrd_t)new_thrd;
 
   // push to ready queue
-  thrd_enqueue(new_thread, &rdy_queue_hd, &rdy_queue_tl);
+  thrd_enqueue(new_thrd, &rdy_queue_hd, &rdy_queue_tl);
 
   return THRD_SUCCESS;
 }
 
 void thrd_exit(void) {
   // make the exiting thread dead
-  curr_thread->state = THRD_DEAD;
+  curr_thrd->state = THRD_DEAD;
 
-  tcb_t* awake_thread = thrd_dequeue(&curr_thread->join_queue_hd, &curr_thread->join_queue_tl);
+  tcb_t* awake_thrd = thrd_dequeue(&curr_thrd->join_queue_hd, &curr_thrd->join_queue_tl);
 
   // set all the joined threads to ready so they can run
-  while (awake_thread != NULL) {
-    awake_thread->state = THRD_READY;
-    thrd_enqueue(awake_thread, &rdy_queue_hd, &rdy_queue_tl);
+  while (awake_thrd != NULL) {
+    awake_thrd->state = THRD_READY;
+    thrd_enqueue(awake_thrd, &rdy_queue_hd, &rdy_queue_tl);
 
-    awake_thread = thrd_dequeue(&curr_thread->join_queue_hd, &curr_thread->join_queue_tl);
+    awake_thrd = thrd_dequeue(&curr_thrd->join_queue_hd, &curr_thrd->join_queue_tl);
   }
   
   thrd_yield();
 }
 
 void thrd_join(thrd_t thread) {
-  tcb_t* cast_thread = (tcb_t*)thread;
+  tcb_t* cast_thrd = (tcb_t*)thread;
 
   // if thread is dead, return
-  if (cast_thread->state == THRD_DEAD)
+  if (cast_thrd->state == THRD_DEAD)
     return;
 
   // mark it as blocked
-  curr_thread->state = THRD_BLOCKED;
+  curr_thrd->state = THRD_BLOCKED;
 
-  // append it to 'cast_thread's join queue
-  thrd_enqueue(curr_thread, &cast_thread->join_queue_hd, &cast_thread->join_queue_tl);
+  // append it to 'cast_thrd's join queue
+  thrd_enqueue(curr_thrd, &cast_thrd->join_queue_hd, &cast_thrd->join_queue_tl);
 
   // yield
   thrd_yield();
 }
 
 void thrd_sleep(uint64_t time_ms) {
-  if (curr_thread == NULL)
+  if (curr_thrd == NULL)
     return;
 
   // get absolute os time
@@ -130,15 +131,15 @@ void thrd_sleep(uint64_t time_ms) {
 
   // the head of the sleep queue will be compared against
   // absolute os time to determine if it should wake up
-  curr_thread->wakeup_time = curr_time_ms + time_ms;
-  curr_thread->state = THRD_SLEEPING;
+  curr_thrd->wakeup_time = curr_time_ms + time_ms;
+  curr_thrd->state = THRD_SLEEPING;
 
   // insert the sleeping thread to sleep queue,
   // assuming that before the insertion the list is sorted
   // by wakeup time, insert it in a way that this promise isnt broken
   tcb_t* curr = sleep_queue_hd;
   tcb_t* prev = NULL;
-  while (curr != NULL && curr->wakeup_time < curr_thread->wakeup_time) {
+  while (curr != NULL && curr->wakeup_time < curr_thrd->wakeup_time) {
     prev = curr;
     curr = curr->next;
   }
@@ -146,11 +147,11 @@ void thrd_sleep(uint64_t time_ms) {
   // found position of the newly sleeping thread,
   // insert it
   if (prev != NULL)
-    prev->next = curr_thread;
+    prev->next = curr_thrd;
   else
-    sleep_queue_hd = curr_thread;
+    sleep_queue_hd = curr_thrd;
 
-  curr_thread->next = curr;
+  curr_thrd->next = curr;
 
   thrd_yield();
 }
