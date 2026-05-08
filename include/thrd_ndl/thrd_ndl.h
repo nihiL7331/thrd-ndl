60b2460013559f0fc0aab3f192488b19abcd72dd
nihiL7331 <pujanekp6@gmail.com>
2026-05-08 22:19:20 +0200

    refactor(core/api): move return codes to public

--- include/thrd_ndl/thrd_ndl.h
+++ include/thrd_ndl/thrd_ndl.h
@@ -3,18 +3,17 @@
 
 #include <stdint.h>
 
+// return codes
+#define THRD_SUCCESS 0
+#define THRD_OOM -1
+
 typedef void* thrd_t;
 
 void thrd_exit(void);
-
 void thrd_yield(void);
-
 void thrd_init(void);
-
 int thrd_create(thrd_t* out_thread, void (*func)(void));
-
 void thrd_join(thrd_t thread);
-
 void thrd_sleep(uint64_t time_ms);
 
 #endif
--- src/internal.h
+++ src/internal.h
@@ -2,7 +2,3 @@
 
 // default stack size for each thread
 #define THRD_STACK_SIZE 1024 * 64 // 64 KB
-
-// return codes
-#define THRD_SUCCESS 0
-#define THRD_OOM -1
