bbf3776f43bea73f9c4634d2476a353a6965f851
nihiL7331 <pujanekp6@gmail.com>
2026-05-08 07:43:24 +0200

    feat(core): thread definition

---
+++ src/tcb.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef enum {
+  READY,
+  RUNNING,
+  DEAD,
+} thrd_state_t;
+
+typedef struct tcb {
+  void* rsp;          // current stack pointer
+  thrd_state_t state; // current state of the thread
+  void* bsp;          // base stack pointer
+  size_t stack_size;
+  struct tcb* next;
+} tcb_t;
