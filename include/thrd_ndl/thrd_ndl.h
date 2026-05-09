4e88bfd651500f16499a16f5637ba6e7e93468b2
nihiL7331 <pujanekp6@gmail.com>
2026-05-09 18:47:18 +0200

    chore(core/api): remove mutex struct name (only typedef mutex_t)

--- include/thrd_ndl/thrd_ndl.h
+++ include/thrd_ndl/thrd_ndl.h
@@ -9,7 +9,7 @@
 
 typedef void* thrd_t;
 
-typedef struct mutex {
+typedef struct {
   int is_locked;
   void* wait_queue_hd;
   void* wait_queue_tl;
@@ -23,7 +23,7 @@ typedef struct {
 void thrd_exit(void);
 void thrd_yield(void);
 void thrd_init(void);
-int thrd_create(thrd_t* out_thread, void (*func)(void));
+int  thrd_create(thrd_t* out_thread, void (*func)(void));
 void thrd_join(thrd_t thread);
 void thrd_sleep(uint64_t time_ms);
 
