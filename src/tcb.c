a42706d5def273abb84da9a1f9241c080b0b9d04
nihiL7331 <pujanekp6@gmail.com>
2026-05-09 15:49:44 +0200

    feat(core): arm support
    missing implicit exit/trampoline, todo next

--- src/tcb.c
+++ src/tcb.c
@@ -9,6 +9,8 @@
 
 #ifdef _WIN32
   #define CALLEE_SAVED_REG_CNT 8
+#elif defined(__aarch64__) // no elifdef on C17:[
+  #define CALLEE_SAVED_REG_CNT 12
 #else
   #define CALLEE_SAVED_REG_CNT 6
 #endif
@@ -45,25 +47,37 @@ tcb_t* tcb_init(void (*entry_point)(void)) {
 
   // set up the stack frame.
   // it is platform dependent:
-  // on windows, need to clear 2 more registers
+  // on windows x86: 8 registers,
+  // on linux x86: 6 registers,
+  // on arm: 12 registers
 
   // push the cleanup function
-  uint64_t* cast_ptr = (uint64_t*)tcb->rsp;
-  cast_ptr--;
-  tcb->rsp = (void*)cast_ptr;
-  *cast_ptr = (uint64_t)thrd_exit;
+  uint64_t* stack = (uint64_t*)tcb->rsp;
+  *(--stack) = (uint64_t)thrd_exit;
   
   // push the instruction pointer ('entry_point')
-  cast_ptr--;
-  tcb->rsp = (void*)cast_ptr;
-  *cast_ptr = (uint64_t)entry_point;
+  // on arm, 'ret' doesn't pop the stack,
+  // it looks at x30 register
+#ifdef __aarch64__
+  *(--stack) = 0x0; // dummy for 16B align
+#else
+  *(--stack) = (uint64_t)entry_point;
+#endif
 
   // clear the callee-saved registers
   // %rbx, %rbp, %r12, %r13, %r14, %r15
   // additional %rdi and %rsi on windows
-  cast_ptr -= CALLEE_SAVED_REG_CNT;
-  tcb->rsp = (void*)cast_ptr;
-  memset(tcb->rsp, 0x0, CALLEE_SAVED_REG_CNT * sizeof(void*));
+  // on arm: x19-x30
+  stack -= CALLEE_SAVED_REG_CNT;
+  memset(stack, 0x0, CALLEE_SAVED_REG_CNT * sizeof(void*));
+
+  // if on arm, place 'entry_point' pointer
+  // in the x30 register
+#ifdef __aarch64__
+  stack[1] = (uint64_t)entry_point;
+#endif
+
+  tcb->rsp = (void*)stack;
 
   return tcb;
 }
