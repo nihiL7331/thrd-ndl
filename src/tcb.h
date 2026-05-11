dca76ba4365b0cccc6ed4d7ec75c1f6c02e0ceab
nihiL7331 <pujanekp6@gmail.com>
2026-05-12 00:08:21 +0200

    fix(tcb): remove redundant stack_size var

--- src/tcb.h
+++ src/tcb.h
@@ -15,7 +15,6 @@ typedef struct tcb {
   void* rsp;          // current stack pointer
   thrd_state_t state; // current state of the thread
   void* bsp;          // base stack pointer
-  size_t stack_size;
 
   struct tcb* next;
 
