7db056b28e03e4b341f1a171c9c2f830460ca4e4
nihiL7331 <pujanekp6@gmail.com>
2026-05-13 17:53:54 +0200

    feat(heap): binary heap header definition

---
+++ src/heap.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct {
+  void** data;
+  size_t count;
+  size_t cap;
+  // comparator returns:
+  // < 0 if a should return first,
+  // > 0 if b should return first,
+  // 0 if equal.
+  int (*cmp)(const void* a, const void* b);
+} heap_t;
+
+int heap_new(heap_t* heap, void** storage, size_t cap, int (*cmp_fn)(const void*, const void*));
+int heap_push(heap_t* heap, void* obj);
+void* heap_peek(const heap_t* heap);
+void* heap_pop(heap_t* heap);
