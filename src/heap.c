9725e3a5745af29f73f049fea07315c13510ffd0
nihiL7331 <pujanekp6@gmail.com>
2026-05-13 17:56:08 +0200

    feat(heap): sift_up helper

--- src/heap.c
+++ src/heap.c
@@ -7,6 +7,7 @@
 #define CHILD_L(i) (((i) * 2) + 1)
 #define CHILD_R(i) (((i) * 2) + 2)
 
+static inline void sift_up(heap_t* heap, size_t idx);
 int heap_new(heap_t* heap, void** storage, size_t cap, int (*cmp_fn)(const void*, const void*)) {
   if (cap == 0 || heap == NULL || storage == NULL || cmp_fn == NULL)
     return THRD_EINVAL;
@@ -30,5 +31,18 @@ static inline void heap_swap(heap_t* heap, size_t idx_a, size_t idx_b) {
   void* tmp = heap->data[idx_a];
   heap->data[idx_a] = heap->data[idx_b];
   heap->data[idx_b] = tmp;
+}
+
+static inline void sift_up(heap_t* heap, size_t idx) {
+  while (idx > 0) {
+    size_t par_idx = PARENT(idx);
+
+    if (heap->cmp(heap->data[idx], heap->data[par_idx]) >= 0)
+      break;
+
+    heap_swap(heap, idx, par_idx);
+
+    idx = par_idx;
+  }
 }
 
