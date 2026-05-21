a5f4fa1d5174bb4a934c8e93680fa7fcd04045a6
nihiL7331 <pujanekp6@gmail.com>
2026-05-21 14:15:45 +0200

    refactor(platform): separate windows and unix logic

--- src/platform.c
+++ src/platform.c
@@ -1,31 +1,24 @@
 #include "platform.h"
-#include <stdint.h>
-#include <thrd_ndl/thrd_ndl.h>
-
-#ifdef _WIN32
-  #define NOMINMAX
-  #include <windows.h>
-  #include <memoryapi.h>
-
-  static HANDLE main_thrd;
-  static HANDLE timer_thrd;
-#else
-  #include <sys/mman.h>
-  #include <unistd.h>
-  #include <time.h>
-  #include <signal.h>
-  #include <sys/time.h>
-  #include <stdio.h>
-  #include <string.h>
-#endif
+#include <stdint.h>            // include for 'uint64_t'
+#include <thrd_ndl/thrd_ndl.h> // include for 'thrd_yield'
 
 #define PREEMPT_TIMER_INTERVAL 7331
 static volatile int preempt_cnt = 0;
 
 static inline size_t align_to_page(size_t size);
 
-uint64_t get_os_time(void) {
 #ifdef _WIN32
+
+#define NOMINMAX
+#include <windows.h>
+#include <memoryapi.h>
+
+#define TRAP_FLAG_MASK 0x100ULL
+
+static HANDLE main_thrd;
+static HANDLE timer_thrd;
+
+uint64_t get_os_time(void) {
   static LARGE_INTEGER win_freq = {0};
   if (win_freq.QuadPart == 0)
     QueryPerformanceFrequency(&win_freq);
@@ -34,72 +27,191 @@ uint64_t get_os_time(void) {
   QueryPerformanceCounter(&ticks);
 
   return (ticks.QuadPart * 1000ULL) / win_freq.QuadPart;
-#else
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  return (ts.tv_sec * 1000ULL) + (ts.tv_nsec / 1000000ULL);
-#endif
 }
 
 void os_sleep_ms(uint64_t time_ms) {
-#ifdef _WIN32
   Sleep((DWORD)time_ms);
-#else
-  struct timespec ts;
-  ts.tv_sec = time_ms / 1000ULL;
-  ts.tv_nsec = (time_ms - ts.tv_sec * 1000ULL) * 1000000ULL;
-  nanosleep(&ts, NULL);
-#endif
 }
 
-// OS call for memory page
 void* os_alloc(size_t size) {
-#ifdef _WIN32
-  void* ptr = VirtualAlloc(NULL, align_to_page(size), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-  return ptr;
-#else
-  void* ptr = mmap(NULL, align_to_page(size), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-  return (ptr == MAP_FAILED) ? NULL : ptr;
-#endif
+  return VirtualAlloc(NULL, align_to_page(size), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 }
 
-// OS call for free
 void os_free(void* ptr, size_t size) {
+  (void)size;
+
   if (ptr == NULL) 
     return;
 
-#ifdef _WIN32
-  (void)size;
   VirtualFree(ptr, 0, MEM_RELEASE);
-#else
-  munmap(ptr, align_to_page(size));
-#endif
 }
 
 int protect_page(void* ptr, size_t size) {
-#ifdef _WIN32
   DWORD old_prot = 0;
   BOOL success = (int)VirtualProtect(ptr, align_to_page(size), PAGE_NOACCESS, &old_prot);
   return success ? 0 : -1;
-#else
-  return mprotect(ptr, align_to_page(size), PROT_NONE);
-#endif
 }
 
 size_t page_size(void) {
   static size_t cached_page_size = 0;
+
   if (cached_page_size == 0) {
-#ifdef _WIN32
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
     cached_page_size = (size_t)sysInfo.dwPageSize;
-#else
-    cached_page_size = (size_t)sysconf(_SC_PAGESIZE);
-#endif
   }
+
   return cached_page_size;
 }
+
+LONG WINAPI signal_handler(PEXCEPTION_POINTERS except_info) {
+  if (except_info->ExceptionRecord->ExceptionCode == EXCEPTION_SINGLE_STEP) {
+    except_info->ContextRecord->EFlags &= ~0x100ULL;
+
+    if (preempt_cnt == 0)
+      thrd_yield();
+
+    return EXCEPTION_CONTINUE_EXECUTION;
+  }
+
+  return EXCEPTION_CONTINUE_SEARCH;
+}
+
+DWORD WINAPI timer_loop(LPVOID arg) {
+  (void)arg;
+  while (1) {
+    Sleep(PREEMPT_TIMER_INTERVAL / 1000); // milliseconds
+
+    if (preempt_cnt == 0) {
+      SuspendThread(main_thrd);
+
+      if (preempt_cnt == 0) {
+        CONTEXT ctx;
+        ctx.ContextFlags = CONTEXT_CONTROL;
+        if (GetThreadContext(main_thrd, &ctx)) {
+          ctx.EFlags |= TRAP_FLAG_MASK;
+          SetThreadContext(main_thrd, &ctx);
+        }
+      }
+
+      ResumeThread(main_thrd);
+    }
+  }
+
+  return 0;
+}
+
+void timer_init(void) {
+  DuplicateHandle(GetCurrentProcess(), GetCurrentThread(),
+                  GetCurrentProcess(), &main_thrd, 0,
+                  FALSE, DUPLICATE_SAME_ACCESS);
+                  
+  AddVectoredExceptionHandler(1, signal_handler);
+  timer_thrd = CreateThread(NULL, 0, timer_loop, NULL, 0, NULL);
+}
+
+void preempt_disable(void) {
+  preempt_cnt++;
+}
+
+void preempt_enable(void) {
+  preempt_cnt--;
+}
+
+#else
+
+#include <sys/mman.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+
+void* os_alloc(size_t size) {
+  void* ptr = mmap(NULL, align_to_page(size), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+  return (ptr == MAP_FAILED) ? NULL : ptr;
+}
+
+void os_free(void* ptr, size_t size) {
+  if (ptr == NULL) 
+    return;
+
+  munmap(ptr, align_to_page(size));
+}
+
+int protect_page(void* ptr, size_t size) {
+  return mprotect(ptr, align_to_page(size), PROT_NONE);
+}
+
+size_t page_size(void) {
+  static size_t cached_page_size = 0;
+  if (cached_page_size == 0)
+    cached_page_size = (size_t)sysconf(_SC_PAGESIZE);
+
+  return cached_page_size;
+}
+
+uint64_t get_os_time(void) {
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+
+  return (ts.tv_sec * 1000ULL) + (ts.tv_nsec / 1000000ULL);
+}
+
+void os_sleep_ms(uint64_t time_ms) {
+  struct timespec ts;
+  ts.tv_sec = time_ms / 1000ULL;
+  ts.tv_nsec = (time_ms - (ts.tv_sec * 1000ULL)) * 1000000ULL;
+
+  nanosleep(&ts, NULL);
+}
+
+static void signal_handler(int num) {
+  (void)num;
+  thrd_yield();
+}
+
+void timer_init(void) {
+  struct sigaction action;
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = signal_handler;
+
+  if (sigaction(SIGVTALRM, &action, NULL) == -1) {
+    perror("sigaction failed");
+    _Exit(1);
+  }
+
+  struct itimerval timer;
+  memset(&timer, 0, sizeof(timer));
+  timer.it_value.tv_usec = PREEMPT_TIMER_INTERVAL;
+  timer.it_interval.tv_usec = PREEMPT_TIMER_INTERVAL;
+
+  if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1) {
+    perror("setitimer failed");
+    _Exit(1);
+  }
+}
+
+void preempt_disable(void) {
+  if (preempt_cnt++ == 0) {
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &sigset, NULL);
+  }
+}
+
+void preempt_enable(void) {
+  if (--preempt_cnt == 0) {
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &sigset, NULL);
+  }
+}
+
+#endif
 
 void* os_alloc_stack(size_t usable_size) {
   size_t total_size = usable_size + page_size(); // page_size for guard
@@ -120,107 +232,7 @@ void os_free_stack(void* base_ptr, size_t usable_size) {
   os_free(base_ptr, usable_size + page_size());
 }
 
-void preempt_disable(void) {
-#ifdef _WIN32
-  preempt_cnt++;
-#else
-  if (preempt_cnt++ == 0) {
-    sigset_t sigset;
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGVTALRM);
-    sigprocmask(SIG_BLOCK, &sigset, NULL);
-  }
-#endif
-}
-
-void preempt_enable(void) {
-#ifdef _WIN32
-  preempt_cnt--;
-#else
-  if (--preempt_cnt == 0) {
-    sigset_t sigset;
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGVTALRM);
-    sigprocmask(SIG_UNBLOCK, &sigset, NULL);
-  }
-#endif
-}
-
-#ifdef _WIN32
-  LONG WINAPI signal_handler(PEXCEPTION_POINTERS except_info) {
-    if (except_info->ExceptionRecord->ExceptionCode == EXCEPTION_SINGLE_STEP) {
-      except_info->ContextRecord->EFlags &= ~0x100ULL;
-
-      if (preempt_cnt == 0)
-        thrd_yield();
-
-      return EXCEPTION_CONTINUE_EXECUTION;
-    }
-
-    return EXCEPTION_CONTINUE_SEARCH;
-  }
-
-  DWORD WINAPI timer_loop(LPVOID arg) {
-    (void)arg;
-    while (1) {
-      Sleep(PREEMPT_TIMER_INTERVAL / 1000);
-
-      if (preempt_cnt == 0) {
-        SuspendThread(main_thrd);
-
-        if (preempt_cnt == 0) {
-          CONTEXT ctx;
-          ctx.ContextFlags = CONTEXT_CONTROL;
-          if (GetThreadContext(main_thrd, &ctx)) {
-            ctx.EFlags |= 0x100ULL;
-            SetThreadContext(main_thrd, &ctx);
-          }
-        }
-
-        ResumeThread(main_thrd);
-      }
-    }
-
-    return 0;
-  }
-#else
-static void signal_handler(int num) {
-  (void)num;
-  thrd_yield();
-}
-#endif
-
-void timer_init(void) {
-#ifdef _WIN32
-  DuplicateHandle(GetCurrentProcess(), GetCurrentThread(),
-                  GetCurrentProcess(), &main_thrd, 0,
-                  FALSE, DUPLICATE_SAME_ACCESS);
-  AddVectoredExceptionHandler(1, signal_handler);
-  timer_thrd = CreateThread(NULL, 0, timer_loop, NULL, 0, NULL);
-#else
-  struct sigaction action;
-  memset(&action, 0x0, sizeof(action));
-  action.sa_handler = signal_handler;
-
-  if (sigaction(SIGVTALRM, &action, NULL) == -1) {
-    perror("sigaction failed");
-    _Exit(1);
-  }
-
-  struct itimerval timer;
-  memset(&timer, 0x0, sizeof(timer));
-  timer.it_value.tv_usec = PREEMPT_TIMER_INTERVAL;
-  timer.it_interval.tv_usec = PREEMPT_TIMER_INTERVAL;
-
-  if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1) {
-    perror("setitimer failed");
-    _Exit(1);
-  }
-#endif
-}
-
 static inline size_t align_to_page(size_t size) {
   size_t p_size = page_size();
   return (size + (p_size - 1)) & ~(p_size - 1);
 }
-
